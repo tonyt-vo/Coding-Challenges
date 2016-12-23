d32aa9f4eb361cdd93d119da87cce870ab9c474c
ttvo1022 <tonyt.vo22@gmail.com>
2016-12-23 02:29:39 -0800

    Reddit Coding Challenges
    These will be my attempts to coding challenges on /r/dailyprogrammer.

---
+++ LetterByLetter.java
@@ -0,0 +1,46 @@
+// Tony Vo
+// 12/23/16
+// Coding Challenge #277
+// https://www.reddit.com/r/dailyprogrammer/comments/
+// 5hy8sm/20161212_challenge_295_easy_letter_by_letter/
+
+public class LetterByLetter {
+
+	public static void main(String[] args) {
+		
+//		transform("The quick","Fun cools");
+	}
+	// Given two strings, transform one to the other letter by letter.
+	public static void transform(String one, String two) {
+		if(one.length() != two.length()) {
+			throw new IllegalArgumentException();
+		}
+		System.out.println("String one : " + one);
+		System.out.println("String two : " + two + "\nTRANSFORM\n");
+		System.out.println(two);
+		
+		int index = 0;
+		// Transform to char array for ease of char manipulation.
+		char[] s2 = two.toCharArray();
+		while(!one.equals(two)) {
+			
+			// Change each index.
+			s2[index] = one.charAt(index);
+			String s = "";
+			
+			// Recreate the string
+			for(int i = 0; i < one.length(); i++) {
+				s+= s2[i];
+			}
+			
+			// Increment and print.
+			index++;
+			two = s;
+			System.out.println(two);
+		}
+	}
+
+	
+
+
+}
---
+++ TwelveDaysOfChristmas.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+// Coding Challenge #277
+// Difficulty Level Easy
+// 12/19/2016
+// Trying out Jsoup
+// Still has HTTP error Fetching URL. Status = 429.
+// "https://www.reddit.com/r/dailyprogrammer/comments/5j6ggm/20161219_challenge_296_easy_the_twelve_days_of/"
+
+public class TwelveDaysOfChristmas {
+
+	public static void main (String[] args) throws IOException {
+		try{
+			
+			//Connect to the URL.
+			System.setProperty("http.agent", "Chrome");
+			Document doc = Jsoup.connect("https://www.reddit.com/r/dailyprogrammer/comments/5j6ggm/20161219_challenge_296_easy_the_twelve_days_of/").get();	
+			
+			// Get the html file and strip it of tags.
+			String song = doc.html();
+			String strip = Jsoup.parse(song).text();
+			
+			// Find the starting index of the song and use substring to get the entire song.
+			// Used a character counter online to count the amount of chars in the song.
+			int start = strip.indexOf("On the first day ");
+			System.out.println(strip.substring(start, start+2120));		
+			
+		// Catch the error.	
+		}catch(Exception e){
+			System.out.println("You didn't get premission to enter website");
+		}
+	}
+}
